2f4ffcdcafb8c3cfcae01e5152903320d62650b1
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-18 23:03:40 +0530

    Add image via file-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import myName from './myName';
 import './style.css';
+import Icon from './js.gif';
 
 
 function component() {
@@ -9,6 +10,11 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = myName('Jaspreet singh');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
